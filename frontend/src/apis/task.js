4ea88b2425e4f6a7a5270118dfd1bba345759ae7
raihanalmenata <raihanalmenata@gmail.com>
2023-11-27 13:31:07 +0700

    Added task apis

---
+++ frontend/src/apis/task.js
@@ -0,0 +1,40 @@
+const app_url = 'http://localhost:3000'
+
+export default {
+    add : async (task) => {
+        
+        const route = '/task'
+        const req_url = app_url + route
+
+        const data = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(task),
+        }
+
+        const response = await fetch(req_url,data)
+        const json = await response.json()
+
+        return json
+        
+    },
+    getAll : async () => {
+
+        const route = '/task'
+        const req_url = app_url + route
+      
+        const data = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          }
+        }
+        
+        const response =  await fetch(req_url, data)
+        const json = await response.json()
+  
+        return json
+      }
+}
