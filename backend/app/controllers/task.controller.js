9fb3e7c1d29dee9b766f8a1c9f371e7034328206
raihanalmenata <raihanalmenata@gmail.com>
2023-11-27 09:08:25 +0700

    Added 'insert handler' at task controller

---
+++ backend/app/controllers/task.controller.js
@@ -0,0 +1,27 @@
+const db = require('../../infra/database/inMemory.db')
+const Task = require('../../models/task.model')
+
+module.exports = {
+    insert : (req, res, next) => {
+        try {
+            
+            const { title, description } = req.body
+            const newTask = new Task({title, description})
+            db['tasks'].unshift(newTask)
+            
+            console.log('current DB: ', JSON.stringify(db));
+            
+            return res.status(201).json({
+                success: true,
+                code: 201,
+                message: "Insert new task successfully",
+                data: newTask
+            });
+
+        } catch (err) {
+            if(err.message === 'INVALID_PAYLOAD')
+                err.status = 400
+            next(err)
+        }
+    }
+}
