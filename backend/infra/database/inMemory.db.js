885fd7f345ca1ba249d29dab7da23ca5175889db
raihanalmenata <raihanalmenata@gmail.com>
2023-11-28 14:05:05 +0700

    Added dummy data

--- backend/infra/database/inMemory.db.js
+++ backend/infra/database/inMemory.db.js
@@ -1,5 +1,18 @@
 const inMemoryDB = {
-    'tasks' : []
+    'tasks' : [
+        { id: '-12', title: 'Task 12', description: 'Task 12 Description' },
+        { id: '-11', title: 'Task 11', description: 'Task 11 Description' },
+        { id: '-10', title: 'Task 10', description: 'Task 10 Description' },
+        { id: '-9', title: 'Task 9', description: 'Task 9 Description' },
+        { id: '-8', title: 'Task 8', description: 'Task 8 Description' },
+        { id: '-7', title: 'Task 7', description: 'Task 7 Description' },
+        { id: '-6', title: 'Task 6', description: 'Task 6 Description' },
+        { id: '-5', title: 'Task 5', description: 'Task 5 Description' },
+        { id: '-4', title: 'Task 4', description: 'Task 4 Description' },
+        { id: '-3', title: 'Task 3', description: 'Task 3 Description' },
+        { id: '-2', title: 'Task 2', description: 'Task 2 Description' },
+        { id: '-1', title: 'Task 1', description: 'Task 1 Description' },
+    ]
 }
 
 module.exports = inMemoryDB
