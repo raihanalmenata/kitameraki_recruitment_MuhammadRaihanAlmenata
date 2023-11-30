730dc80f1fdff10893002397d23d2e43be7ac6d9
raihanalmenata <raihanalmenata@gmail.com>
2023-11-30 21:30:24 +0700

    Added docker setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { initializeIcons } from '@fluentui/react/lib/Icons';
+
+initializeIcons();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
